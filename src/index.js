4a51b115c061ecb94ebcea78a0f8bd3ac658f7f2
DaroDax <darwingiralgo0304@gmail.com>
2021-12-27 09:59:44 -0430

    Servidor MONGO

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const socketio = require('socket.io'); //Conexion tiempo real
 const io = socketio(server); //Actualizado
 
 //db connection 
-mongoose.connect('mongodb://localhost/chat-database')
+mongoose.connect('mongodb+srv://nickie:juanjose0304@heroku-chat.wxjls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(db => console.log('DataBase connection sucessfully'))
 .catch(err => console.log(err));
 
